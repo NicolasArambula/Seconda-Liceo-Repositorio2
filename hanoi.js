227baa8d48f5b87b50542b0f92d327cbc5c0b73d
NicolasArambula <92396235+NicolasArambula@users.noreply.github.com>
2022-04-27 19:17:26 -0500

    Add files via upload

---
+++ hanoi.js
@@ -0,0 +1,44 @@
+let respuesta=""
+
+function hanoi(discos,origen,final) {
+    if (discos==1) {
+        respuesta=respuesta + origen + " -> " + final + "\n"
+    } else{
+        if (final==3&&origen==1) {
+            nuevo_final=2
+        }
+        if (final==3&&origen==2) {
+            nuevo_final=1
+        }
+        if (final==2&&origen==1) {
+            nuevo_final=3
+        }
+        if (final==2&&origen==3) {
+            nuevo_final=1
+        }
+        if (final==1&&origen==3) {
+            nuevo_final=2
+        }
+        if (final==1&&origen==2) {
+            nuevo_final=3
+        }
+        hanoi(discos-1,origen,nuevo_final)
+        respuesta= respuesta + origen + " -> " + final + "\n"
+        nuevo_origen = nuevo_final
+        nuevo_final=final
+        hanoi(discos-1, nuevo_origen, nuevo_final)
+    }
+}
+
+do {
+    discos=Number(prompt("Cuantos discos?"))    
+} while (discos>10|| discos<1||isNaN(discos));
+do {
+    origen=Number(prompt("Cual es el orgiend de los discos?"))    
+} while (origen!=1 && origen!=2 && origen!=3 || isNaN(origen));
+do {
+    final=Number(prompt("Cual es el destino de los discos?"))    
+} while (final!=1 && final!=2 && final!=3 || isNaN(final)||final==origen);
+
+hanoi(discos,origen,final)
+console.log(respuesta)
